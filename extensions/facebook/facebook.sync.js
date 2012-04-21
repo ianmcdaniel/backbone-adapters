a7b62d04442c6fa5eff3558d94632b301243c8d1
Ian McDaniel <ianmcdaniel@gmail.com>
2012-04-21 10:36:41 -0700

    facebook demo app

--- extensions/facebook/facebook.sync.js
+++ extensions/facebook/facebook.sync.js
@@ -1,6 +1,48 @@
-// Backbone Sync Extension
+// Backbone Sync Extension - Facebook API
 // https://github.com/ianmcdaniel/Backbone-Sync-Extensions
 
+  FacebookSync = function(method, model, options) {
+    var callback, type, params = {};
+    type = {
+      'create': 'post',
+      'update': 'post',
+      'delete': 'delete',
+      'read':   'get'
+    }[method];
 
+    options || (options = {});
 
-FacebookSync = function(){}
+    if(model && model.url) {
+      params.url = (_.isFunction(model.url)) ? model.url(method) : model.url;
+    } else if (!options.url) {
+      throw new Error('A "url" property or function must be specified');
+    }
+
+    if (!options.data && model && (method == 'create' || method == 'update')) {
+      params.data = JSON.stringify(model);
+    }
+    
+    params = _.extend(params, options);
+    
+    // make sure error and success functions get called
+    callback = function(res) {
+      if (!res) return;
+      if (res.error) {
+        if (options.error && _.isFunction(options.error)) {
+          return options.error(res);
+        }
+      } else {
+        if (options.success && _.isFunction(options.success)) {
+          return options.success(res);
+        }
+      }
+    };
+    
+    // Make the request to facebook using the facebook js sdk
+    if(type == 'get') {
+      return FB.api(params.url,params,callback)
+    } else {
+      return FB.api(params.url,type,params,callback)
+    }
+    
+  };
