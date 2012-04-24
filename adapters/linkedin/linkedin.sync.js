dc3a778b16497dc0359639e325955c3be99cad64
Ian McDaniel <ianmcdaniel@gmail.com>
2012-04-24 10:36:26 -0700

    added linkedin

--- adapters/linkedin/linkedin.sync.js
+++ adapters/linkedin/linkedin.sync.js
@@ -1,7 +1,8 @@
-// Backbone Adapters - Facebook API
+// Backbone Adapters - LinkedIn API
 // https://github.com/ianmcdaniel/Backbone-Adapters
 
 (function() {
+
 
   var getValue = function(object,prop) {
     if (!(object && object[prop])) return null;
@@ -12,48 +13,53 @@
     throw new Error('A "url" property or function must be specified');
   };
 
-  // A Backbone.sync Adapter for the facebook graph api
-  var FacebookSync = function(method, model, options) {
-    var url, data= {}, type, callback;
+  // A Backbone.sync Adapter for the linkedin api
+  var LinkedInSync = function(method, model, options) {
+    var url, data= {}, type, fields = "", query = "", callback;
 
     options || (options = {});
     type = {
-      'create': 'post',
-      'update': 'post',
-      'delete': 'delete',
-      'read':   'get'
+      'create': 'POST',
+      'update': 'PUT',
+      'delete': 'DELETE',
+      'read':   'GET'
     }[method];
 
     if(options.type) {
       type = options.type
     }
-        
-    if(!options.url) {    
-      if(getValue(model.attributes, 'id') && model.url === Backbone.Model.prototype.url) {
-        // if we have an id, we dont need a url
-        url = getValue(model.attributes, 'id'); 
-      } else {
-        url = getValue(model, 'url') || urlError();
-      }
+    
+    if (!options.url) {
+      url = getValue(model, 'url') || urlError();
     } else {
       url = options.url;
     }
     
+    if (options.fields) {
+      fields = ":(" + options.fields + ")";
+    }
+        
     // if a collection's parse method has not been changed then
-    // create one that works with facebook data results
+    // create one that works with linkedin data
     if(model && model.parse && model.parse === Backbone.Collection.prototype.parse) {
-      model.parse = function(resp) {return resp.data}
+      model.parse = function(resp) {return resp.values}
     }
 
     if (!options.data && model && (method == 'create' || method == 'update')) {
       data = model.toJSON();
     }
     _.extend(data, options.data);
+    
+    if(type == 'GET' && !_.isEmpty(data)) {
+      query = "?" + $.param(data);
+      data = {};
+    }
+    data = JSON.stringify(data);
 
     // make sure error and success functions get called
     callback = function(res) {
       if (!res) return;
-      if (res.error) {
+      if (res.errorCode) {
         if (options.error && _.isFunction(options.error)) {
           return options.error(res);
         }
@@ -64,16 +70,12 @@
       }
     };
     
-    // Make the request to facebook using the facebook js sdk
-    if(type == 'get') {
-      return FB.api(url,data,callback)
-    } else {
-      return FB.api(url,type,data,callback)
-    }
+    // Make the request to linkedin using the linkedin js sdk
+    IN.API.Raw(url + fields + query).method(type).body(data).result(callback);
     
   };
   
   // attach it to Backbone namespace
-  Backbone.FacebookSync = FacebookSync;
+  Backbone.LinkedInSync = LinkedInSync;
 
 })();
